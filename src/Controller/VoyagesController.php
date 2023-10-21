2334f9b5a585ad0514882a632c2f450dfcdbac72
Nessprog <ineslaniez31@gmail.com>
2023-10-21 22:22:11 +0200

    Phase 11 : ORM (nouvelle page pour l'affichage détaillé d'un enregistrement)

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -67,5 +67,17 @@ class VoyagesController extends AbstractController {
             'visites' => $visites
         ]);
     }
+    
+    /**
+     * @Route("/voyages/voyage/{id}", name="voyages.showone")
+     * @param type $id
+     * @return Repsonse
+     */
+    public function showOne($id): Response{
+        $visite = $this->repository->find($id);
+        return $this->render("pages/voyage.html.twig", [
+            'visite' => $visite
+        ]);
+    }
   
 }
