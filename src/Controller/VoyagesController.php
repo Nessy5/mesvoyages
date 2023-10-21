7d52471c15da0e44722531074980298cac9abb4b
Nessprog <ineslaniez31@gmail.com>
2023-10-21 15:25:42 +0200

    Phase 9 : ORM (tri des enregistrements)

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -21,7 +21,7 @@ class VoyagesController extends AbstractController {
      * @return Response
      */
     public function index() : Response{
-        $visites = $this->repository->findAll();
+        $visites = $this->repository->findAllOrderBy('datecreation', 'DESC');
         return $this->render("pages/voyages.html.twig", [
             'visites' => $visites
         ]);
@@ -40,5 +40,56 @@ class VoyagesController extends AbstractController {
     public function __construct(VisiteRepository $repository) {
         $this->repository = $repository;
     }
+    
+    /**
+     * @Route("/voyages/tri/{champ}/{ordre}", name="voyages.sort")
+     * @param type $champ
+     * @param type $ordre
+     * @return Response
+     */
+    public function sort($champ, $ordre): Response{
+        $visites = $this->repository->findAllOrderBy($champ, $ordre);
+        return $this->render("pages/voyages.html.twig", [
+            'visites' => $visites
+        ]);
+    }
+    
+    /**
+     * @Route("/voyages/tri/{champ}/{ordre}", name="pays.sort")
+     * @param type $champ
+     * @param type $ordre
+     * @return Response
+     */
+    public function sort2($champ, $ordre): Response{
+        $visites = $this->repository->findAllOrderBy($champ, $ordre);
+        return $this->render("pages/voyages.html.twig", [
+            'visites' => $visites
+        ]);
+    }
+    
+    /**
+     * @Route("/voyages/tri/{champ}/{ordre}", name="note.sort")
+     * @param type $champ
+     * @param type $ordre
+     * @return Response
+     */
+    public function sort3($champ, $ordre): Response{
+        $visites = $this->repository->findAllOrderBy($champ, $ordre);
+        return $this->render("pages/voyages.html.twig", [
+            'visites' => $visites
+        ]);
+    }
+    
+    /**
+     * @Route("/voyages/tri/{champ}/{ordre}", name="date.sort")
+     * @param type $champ
+     * @param type $ordre
+     * @return Response
+     */
+    public function sort4($champ, $ordre): Response{
+        $visites = $this->repository->findAllOrderBy($champ, $ordre);
+        return $this->render("pages/voyages.html.twig", [
+            'visites' => $visites
+        ]);
+    }
 }
-
